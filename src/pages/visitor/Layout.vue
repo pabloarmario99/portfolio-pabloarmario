66c5ccbf5772554b100d4fc20a4592091dd90cc9
pabloarmario99 <pabloarmariogil@gmail.com>
2026-02-25 17:20:47 +0100

    Layout ruta vuelta a home implementada

--- src/pages/visitor/Layout.vue
+++ src/pages/visitor/Layout.vue
@@ -3,9 +3,28 @@
 </script>
 
 <template>
-    <div>
-        <header>Constante¿?</header>
-        <main><RouterView/></main>
+    <div class="grid grid-cols-[auto_1fr] min-w-screen min-h-screen font-sans">
+        <header>
+            <div class="flex flex-col gap-2">
+            <RouterLink to="/home">
+                Home
+            </RouterLink>
+            <RouterLink to="/visitor/projects" active-class="font-bold">
+                Projects
+            </RouterLink>
+        
+            <RouterLink to="/visitor/about" active-class="font-bold">
+                About
+            </RouterLink>
+        
+            <RouterLink to="/visitor/contact" active-class="font-bold">
+                Contact
+            </RouterLink>
+        </div>
+        </header>
+        <main class="ml-10 bg-gray-100">
+            <RouterView/>
+        </main>
     </div>
 </template>
 
