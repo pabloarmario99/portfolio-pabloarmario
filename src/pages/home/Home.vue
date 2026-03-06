067f2cb313a2d19a68650f68fa904b3d45ed02a1
pabloarmario99 <pabloarmariogil@gmail.com>
2026-03-06 19:21:09 +0100

    Mejora home: ajustes del header y animaciones

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -5,7 +5,7 @@ import { onBeforeUnmount, onMounted, ref } from 'vue';
 const compactHeader = ref(false);
 
 const onScroll = () => {
-    compactHeader.value = window.scrollY > 80;
+    compactHeader.value = window.scrollY > 50;
 };
 
 onMounted(() => {
@@ -25,48 +25,45 @@ onBeforeUnmount(() => {
     <header
     :class="[
       'fixed top-0 left-0 w-full z-10 bg-black text-white  transition-all duration-100',
-      compactHeader ? 'py-2' : 'py-2'
+      compactHeader ? 'pt-2' : 'pt-2'
     ]">
       
-        <div class="max-w-6xl mx-auto px-4 gap-2 flex flex-col justify-center items-center">
+        <div class="gap-2 flex flex-col items-center">
             <h1 :class="compactHeader ? 'text-base' : 'text-2xl'" class="font-medium transition-all duration-100">
-                Portfolio: Argi
+                Porfolio: Argi
             </h1>
-            <img 
-            src="/images/Argi_logo.png"
-            v-if="!compactHeader" class="h-auto w-20 sm:w-40 transition-all ">
+                <img 
+                src="/images/Argi_logo.png"
+                v-if="!compactHeader" 
+                class="h-auto w-20 sm:w-25 transition-all slow-spin">
+            
+                <p v-if="!compactHeader" class="font-light text-xs flex justify-center lg:justify-start sm:text-sm text-gray-300 mt-1">
+                    Creación de historias, mundos y personajes
+                </p>
 
-            <p v-if="!compactHeader" class="text-sm text-gray-300 mt-1">
-                Ilustración, personajes y proyectos
-            </p>
-
-            <nav class="flex gap-1 font-light">
-                <RouterLink to="/visitor/projects" class="bg-black text-white px-2 py-1 hover:bg-gray-900">
-                    Proyectos
-                </RouterLink>
-                <RouterLink to="/visitor/character" class="bg-black text-white px-2 py-1 hover:bg-gray-900">
-                    Personajes
-                </RouterLink>
-            
-            
-                <RouterLink to="/visitor/about" class="bg-black text-white px-1 py-1 hover:bg-gray-900">
-                    Sobre mi
-                </RouterLink>
-            
-        
-                <RouterLink to="/visitor/contact" class="bg-black text-white px-2 py-1 hover:bg-gray-900">
-                    Contacta
-                </RouterLink>
-            </nav>
+                <nav class="flex gap-2 sm:gap-8 text-sm sm:text-base justify-center font-light transition-all">
+                    <RouterLink to="/visitor/projects" class="bg-black text-white px-2 py-1 pb-2 hover:bg-[#202020] rounded-t-sm">
+                        Proyectos
+                    </RouterLink>
+                    <RouterLink to="/visitor/character" class="bg-black text-white px-2 py-1 pb-2 hover:bg-[#202020] rounded-t-sm">
+                        Personajes
+                    </RouterLink>
+                    <RouterLink to="/visitor/about" class="bg-black text-white px-2 py-1 pb-2 hover:bg-[#202020] rounded-t-sm">
+                        Sobre mi
+                    </RouterLink>
+                    <RouterLink to="/visitor/contact" class="bg-black text-white px-2 py-1 pb-2 hover:bg-[#202020] rounded-t-sm">
+                        Contacta
+                    </RouterLink>
+                </nav>
         </div>
     </header>
 
-    <main :class="compactHeader ? 'pt-44 sm:pt-48' : 'pt-52 sm:pt-75'" class="transition-all duration-100">
+    <main :class="compactHeader ? 'pt-46 sm:pt-48' : 'pt-52 sm:pt-61'" class="transition-all duration-100 px-4">
         <section class="columns-1 sm:columns-2 lg:columns-3 gap-4 [column-fill:balance]">
-        <img src="/images/ME_escena1.jpg" class="mb-4 w-full h-auto  break-inside-avoid" />
-        <img src="/images/GN_portadaAmaya.jpg" class="mb-4 w-full h-auto  break-inside-avoid" />
-        <img src="/images/GN_portadaGuardian.jpg" class="mb-4 w-full h-auto  break-inside-avoid" />
-        <img src="/images/JC_portada.jpg" class="mb-4 w-full h-auto  break-inside-avoid" />
+        <img src="/images/ME_escena1.jpg" class="mb-4 w-full h-auto break-inside-avoid transition-transform duration-200 ease-out hover:scale-103" />
+        <img src="/images/GN_portadaAmaya.jpg" class="mb-4 w-full h-auto break-inside-avoid transition-transform duration-200 ease-out hover:scale-103" />
+        <img src="/images/GN_portadaGuardian.jpg" class="mb-4 w-full h-auto break-inside-avoid transition-transform duration-200 ease-out hover:scale-103" />
+        <img src="/images/JC_portada.jpg" class="mb-4 w-full h-auto break-inside-avoid transition-transform duration-200 ease-out hover:scale-103" />
     </section>
     </main>
 
@@ -76,5 +73,17 @@ onBeforeUnmount(() => {
 </template>
 
 <style scoped>
+.slow-spin {
+  animation: slow-spin 12s linear infinite;
+}
+
+@keyframes slow-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
 
 </style>
